8559a341dfd4387daf97d666d895cf6a8704d7a3
TimDTTP <timdttp@gmail.com>
2024-10-04 17:53:41 -0500

    Update: changed settings
    - numberwidth 4 -> 2

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -19,7 +19,7 @@ vim.o.incsearch = true -- Set incremental search on
 vim.o.showmode = false -- We don't need to see things like -- INSERT -- anymore (default: true)
 vim.opt.termguicolors = true -- Set termguicolors to enable highlight groups (default: false)
 vim.o.whichwrap = "bs<>[]hl" -- Which "horizontal" keys are allowed to travel to prev/next line (default: 'b,s')
-vim.o.numberwidth = 4 -- Set number column width to 2 {default 4} (default: 4)
+vim.o.numberwidth = 2 -- Set number column width to 2 {default 4} (default: 4)
 vim.o.swapfile = false -- Creates a swapfile (default: true)
 vim.o.smartindent = true -- Make indenting smarter again (default: false)
 vim.o.showtabline = 1 -- Always show tabs (default: 1)
@@ -29,5 +29,5 @@ vim.o.conceallevel = 0 -- So that `` is visible in markdown files (default: 1)
 vim.opt.formatoptions:remove({ "c", "r", "o" }) -- Don't insert the current comment leader automatically for auto-wrapping comments using 'textwidth', hitting <Enter> in insert mode, or hitting 'o' or 'O' in normal mode. (default: 'croql')
 vim.o.background = "dark"
 vim.o.mousehide = true
-vim.o.foldcolumn = "2"
+vim.o.foldcolumn = "0"
 vim.o.cmdheight = 0
